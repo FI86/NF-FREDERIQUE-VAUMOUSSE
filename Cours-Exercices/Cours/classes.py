d86c2a103e5acbb8dfabf8bd180eae99a70071d6
Twixy Twixy <twixy@free.fr>
2025-04-25 08:56:33 +0200

    cours exercices

---
+++ Cours-Exercices/Cours/classes.py
@@ -0,0 +1,21 @@
+# Fichier d'exemples pour les classes
+
+# Classe avec constructeur
+    # Attribut de classe (utiliser le nom de la classe ou cls)
+    # Attention car utilisable comme un attribut d'objet selon le prefixe utilise
+
+    # Constructeur
+        # Attributs d'instances (objets)
+
+    # Methode de classe
+
+    # Methode static. cette methode n'utilise ni cls ni self.
+    # Bonne pratique : prefixe l'appel de la fonction avec le nom de la classe.
+
+# Exemple Rectangle avec heritage de classe
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ Cours-Exercices/Cours/module_pathlib.py
@@ -0,0 +1,19 @@
+# Fichier d'exemples avec le module pathlib
+
+# Imports
+
+def main():
+    pass
+    # Afficher le chemin courant, le nom de l'OS
+
+    # Vérification de l'existence d'un élément et de son type
+
+    # Manipuler les informations sur le chemin du fichier
+
+    # Obtenir la date de modification du fichier
+    
+    # Calculer le temps écoulé depuis la dernière modification
+
+
+if __name__ == "__main__":
+    main()
